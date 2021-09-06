4ad38cb062c39617cf1bd0781374a6fce2e80cb2
andmau4 <75027349+andmau4@users.noreply.github.com>
2021-09-06 09:33:37 -0500

    Add files via upload

---
+++ js/test_1JS.js
@@ -0,0 +1,11 @@
+function start(){
+    const canvas=
+    document.querySelector("#glCanvas");
+    const gl=canvas.getContext("webgl");
+    if (!gl) {
+        alert("El navegador no es compatible con WEBGL")
+        return;
+    }
+    gl.clearColor(0.0,0.0,0.0,1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
